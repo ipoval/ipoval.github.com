714a34743d2ce0b4d5f61842405d92a087b66a64
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-06-10 14:58:45 -0700

    added mylib.js

---
+++ assets/javascripts/mylib.js
@@ -0,0 +1,28 @@
+// jQuery(function($) { $('.edit_task').submitOnCheck(); })
+//
+jQuery.fn.submitOnCheck = function() {
+  $(this).find('input[type=submit]').remove();
+  $(this).find('input[type=checkbox]').click(function() {
+    $(this).parent('form').submit();
+  });
+}
+
+// String monkepatches
+
+// "string".trim
+//
+if (!String.prototype.trim) {
+  String.prototype.trim = function() {
+    return this.replace(/^\s+|\s+$/g, '');
+  }
+}
+
+// "string".word_count
+//
+if (!String.prototype.word_count) {
+  String.prototype.word_count = function() {
+    return this.split(/\s+/).length;
+  }
+}
+
+// \String monkepatches
