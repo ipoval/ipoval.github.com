2ab626e6e22e6d0e5cb3c32068ad8a5cf9fa80be
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-07-29 22:32:05 -0700

    added image

--- ipova.s3-website-us-west-1.amazonaws.com/s3push.rb
+++ ipova.s3-website-us-west-1.amazonaws.com/s3push.rb
@@ -5,8 +5,10 @@
 if __FILE__ == $0
 
 require 'pathname'
+require 'logger'
 require 'aws'
 
+AWS.config(logger: Logger.new($stdout), log_level: :debug)
 s3 = AWS::S3.new(
   access_key_id: 'AKIAI42GB3TAHPEU2VWA',
   secret_access_key: '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'
@@ -18,8 +20,8 @@ git_log = `git log -1 --pretty="format:" --name-only .`
 uploading_files = git_log.split($/).compact.reject(&:empty?)
 
 uploading_files.each do |f|
-  puts Pathname.new(f).to_s
-  # bucket[f].write Pathname.new(f)
+  path = f.sub('ipova.s3-website-us-west-1.amazonaws.com/', '')
+  bucket.objects[path].write Pathname.new(path)
 end
 
 end
