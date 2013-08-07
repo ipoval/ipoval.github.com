b9f561107201cfb822588c4924660ed1ddad2065
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-08-07 10:47:20 -0700

    rm the sub-folder according to the sub-bucket name

--- photos.ipoval.com/s3push.rb
+++ photos.ipoval.com/s3push.rb
@@ -8,19 +8,18 @@ require 'pathname'
 require 'logger'
 require 'aws'
 
-AWS.config(logger: Logger.new($stdout), log_level: :debug)
-s3 = AWS::S3.new(
-  access_key_id: 'AKIAI42GB3TAHPEU2VWA',
-  secret_access_key: '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'
-)
+PHOTOS_S3_BUCKET_NAME = 'photos.ipoval.com'
 
-bucket = s3.buckets['ipova']
+AWS.config(logger: Logger.new($stdout), log_level: :debug)
+s3 = AWS::S3.new(access_key_id: 'AKIAI42GB3TAHPEU2VWA', secret_access_key: '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')
+
+bucket = s3.buckets[PHOTOS_S3_BUCKET_NAME]
 
 git_log = `git log -1 --pretty="format:" --name-only .`
 uploading_files = git_log.split($/).compact.reject(&:empty?)
 
 uploading_files.each do |f|
-  path = f.sub('ipova.s3-website-us-west-1.amazonaws.com/', '')
+  path = f.sub("#{PHOTOS_S3_BUCKET_NAME}/", '')
   bucket.objects[path].write Pathname.new(path) if Pathname.new(path).exist?
 end
 
