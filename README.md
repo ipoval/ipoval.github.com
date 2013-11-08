4c9aa19ba11c91998f4fad0b2fb31bebfb161c42
Ivan Povalyukhin <ivanpoval@gmail.com>
2013-11-08 15:00:38 -0800

    update the manifiset and use strict mode in JS

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 http://ipoval.com
 ================
 
-chrome app
-================
+## chrome app
 - add to chrome web store (https://chrome.google.com/webstore/detail/nmfpplkdkcbhediajmbhljkafnlahcda)
 
-## Screenshot
 ![screenshot](https://dl.dropboxusercontent.com/u/8180988/ipovalchromeapp.png)
 
 @ipoval
--- chromeapp/background.js
+++ chromeapp/background.js
@@ -1,8 +1,24 @@
+"use strict";
+
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.runtime.html
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
 chrome.app.runtime.onLaunched.addListener(function() {
+  var screenWidth  = screen.availWidth,
+      screenHeight = screen.availHeight,
+      width        = 1200,
+      height       = 768;
+
   chrome.app.window.create('index.html', {
-    'bounds': {
-      'width': 1200,
-      'height': 768
+    id: '@ipoval-chromeapp',
+    bounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth - width) / 2),
+      top: Math.round((screenHeight - height) / 2)
     }
   });
 });
