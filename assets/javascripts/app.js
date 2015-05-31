6698404b673193bbf968d326e8c74dabfbdb5f0f
Ivan Povalyukhin <ipovalyukhin@yammer-inc.com>
2015-05-30 19:47:08 -0700

    load twitter feed later

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -128,10 +128,12 @@ jQuery(function($) {
       addthis.layers({ 'theme' : 'transparent', 'share' : { 'position' : 'right', 'numPreferredServices' : 5 }, 'follow' : { 'services' : [ {'service': 'twitter', 'id': 'ipoval'}, {'service': 'linkedin', 'id': 'ipoval'} ] } });
     });
 
-    /* load twitter feed widget */
+    /* load twitter feed widget */    
+    
     (function(d,s,id){
       var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";js.async=true;fjs.parentNode.insertBefore(js,fjs);}
     })(document,"script","twitter-wjs");
+    
   }, 2500);
 });
 
