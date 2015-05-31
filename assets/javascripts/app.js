9d9b498b3e0c48231dc5fab026a9e8e1b49129b5
Ivan Povalyukhin <ipovalyukhin@yammer-inc.com>
2015-05-30 19:01:44 -0700

    improve Critical Rendering Path by elimitating extra Send Request and JS eval that blocks DOM rendering

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -95,6 +95,82 @@ jQuery(function($) {
       });
     }
   };
-  setTimeout(getCurrentGeolocation, 7000);
+  setTimeout(getCurrentGeolocation, 5000);
 
+  (function animateSidebar() {
+    'use strict';
+    var sidebarSections = $('div.sidebar-nav .ip-nav-header'),
+      worker = new Worker('assets/javascripts/worker.js');
+    worker.onmessage = function(event) {
+      this.callCount = this.callCount || 0;
+      this.callCount++;
+      // console.dir(event.data); console.dir(this.callCount);
+      var deg = event.data.randomNum2,
+        randomSidebar = sidebarSections.eq(event.data.randomNum1);
+      randomSidebar.css({
+        '-webkit-transform': 'rotate(-' + deg + 'deg)',
+        '-moz-transform': 'rotate(-' + deg + 'deg)',
+        'transform': 'rotate(-' + deg + 'deg)',
+      });
+    };
+    worker.onerror = function(event) {
+      console.dir(event);
+    };
+    setInterval(function() {
+      worker.postMessage({ randomMax: sidebarSections.size() + 1 });
+    }, 1500);
+  })();
+
+  /*
+   * loadScript("js.js", function() { Application.init(); });
+   * non-blocking pattern to load script
+   * embedded here, thus avoiding another HTTP request
+   */
+  function loadScript(url, callback) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    if (callback) {
+      if (script.readyState) { /* IE */
+        script.onreadystatechange = function() {
+          if (script.readyState == "loaded" || script.readyState == "complete") {
+            script.onreadystatechange = null;
+            callback();
+          }
+        };
+      } else {
+        script.onload = function() { callback(); };
+      }
+    }
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+
+  setTimeout(function() {
+    loadScript('http://cdn.jsdelivr.net/qtip2/2.2.1/jquery.qtip.js', function() {
+      $('[data-tooltip!=""]').qtip({ content: { attr: 'data-tooltip' }, style: { classes: 'qtip-bootstrap' } });
+    });
+    loadScript('//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52012df744920f69', function() {
+      addthis.layers({ 'theme' : 'transparent', 'share' : { 'position' : 'right', 'numPreferredServices' : 5 }, 'follow' : { 'services' : [ {'service': 'twitter', 'id': 'ipoval'}, {'service': 'linkedin', 'id': 'ipoval'} ] } });
+    });
+  }, 2500);
 });
+
+/*!
+ * IE10 viewport hack for Surface/desktop Windows 8 bug
+ * Copyright 2014 Twitter, Inc.
+ * Licensed under the Creative Commons Attribution 3.0 Unported License. For
+ * details, see http://creativecommons.org/licenses/by/3.0/.
+ */
+/* http://getbootstrap.com/getting-started/#support-ie10-width */
+(function () {
+  'use strict';
+  if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+    var msViewportStyle = document.createElement('style')
+    msViewportStyle.appendChild(
+      document.createTextNode(
+        '@-ms-viewport{width:auto!important}'
+      )
+    )
+    document.querySelector('head').appendChild(msViewportStyle)
+  }
+})();
