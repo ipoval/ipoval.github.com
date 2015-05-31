28cab16a879113fc5b75ca04a654a3f46165fa43
Ivan Povalyukhin <ipovalyukhin@yammer-inc.com>
2015-05-30 19:49:00 -0700

    load twitter feed later

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -129,7 +129,6 @@ jQuery(function($) {
     });
 
     /* load twitter feed widget */    
-    
     (function(d,s,id){
       var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";js.async=true;fjs.parentNode.insertBefore(js,fjs);}
     })(document,"script","twitter-wjs");
